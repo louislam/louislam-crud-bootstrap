03f474989337f26ee0b2f9fcc95b918567754427
Louis Lam <louislam@live.com>
2015-08-15 12:10:35 +0800

    init with source

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+require "vendor/autoload.php";
+
+use LouisLam\CRUD\SlimLouisCRUD;
+use RedBeanPHP\R;
+
+
+/*
+ * 1. Setup a Database Connection (Support MySQL, SQLite etc.)
+ * Please refer to: http://www.redbeanphp.com/connection
+ */
+R::setup('sqlite:dbfile.db');
+
+
+/*
+ * 2. Create a SlimLouisCRUD instance.
+ */
+$crud = new SlimLouisCRUD();
+
+
+/*
+ * 3. Add handlers
+ */
+$crud->add("user", function () use ($crud) {
+    $crud->showFields("id", "name", "password", "email");
+});
+
+$crud->add("book", function () use ($crud) {
+    $crud->showFields("id", "name", "password", "email");
+});
+
+
+/*
+ * 4. Run the application
+ */
+$crud->run();
+
